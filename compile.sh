c94ba4446e02b61d706abe15b2aeae54ad2fb7a3
shhhQuiettt <krzysztof.skro@gmail.com>
2023-04-22 18:10:51 +0200

    Fix valgrind errors

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 gcc -c bmp_header_utils.c -o bmp_header_utils.o 
-gcc -c main.c -o main.o && gcc main.o bmp_header_utils.o -Wextra -Wall -o main && valgrind --leak-check=full ./main
+gcc -c main.c -o main.o && gcc main.o bmp_header_utils.o -Wextra -Wall -o main && valgrind --leak-check=full --show-leak-kinds=all ./main
+
+
+
--- main.c
+++ main.c
@@ -9,8 +9,8 @@ struct CmdArgs {
 };
 
 struct Pixel {
-  uint8_t red;
   uint8_t green;
+  uint8_t red;
   uint8_t blue;
 };
 
@@ -23,18 +23,29 @@ FILE *open_file(char *filename, char *mode) {
   return fp;
 }
 
-void grayLine(uint8_t *lineBuffer, size_t bytesToGray) {
+uint8_t *grayLine(uint8_t *lineBuffer, size_t bytesToGray, size_t fullRowSize) {
   if (bytesToGray % 3 != 0) {
     fprintf(stderr, "Error: amount of bytes to gray is not divisible by 3");
     exit(1);
   }
 
-  for (int i = 0; i + 3 <= bytesToGray; i += 3) {
-      uint8_t pixelAvg = (lineBuffer[i] + lineBuffer[i + 1] + lineBuffer[i + 2]) / 3;
-      lineBuffer[i] = pixelAvg;
-      lineBuffer[i + 1] = pixelAvg;
-      lineBuffer[i + 2] = pixelAvg;
+  uint8_t *grayed_buffer = malloc(sizeof(uint8_t) * fullRowSize);
+  int i = 0;
+  while (i < bytesToGray) {
+    uint8_t pixelAvg =
+        (lineBuffer[i] + lineBuffer[i + 1] + lineBuffer[i + 2]) / 3;
+    grayed_buffer[i] = pixelAvg;
+    grayed_buffer[i + 1] = pixelAvg;
+    grayed_buffer[i + 2] = pixelAvg;
+    i += 3;
   }
+
+  while (i < fullRowSize) {
+    grayed_buffer[i] = 0;
+    i++;
+  }
+
+  return grayed_buffer;
 }
 
 int main(int argc, char *argv[]) {
@@ -44,32 +55,43 @@ int main(int argc, char *argv[]) {
 
   printf("a");
 
-  FILE *in_fp = open_file(filename, "rb");
-  FILE *out_fp = open_file(output_filename, "wb");
+  /* FILE *in_fp = open_file(filename, "rb"); */
+  /* FILE *out_fp = open_file(output_filename, "wb"); */
+  FILE *in_fp = fopen(filename, "rb");
+  FILE *out_fp = fopen(output_filename, "wb");
 
   BITMAPFILEHEADER *header = read_bmp_header(in_fp);
   BITMAPINFOHEADER *info_header = read_bmp_info_header(in_fp);
 
   print_bmp_header(header);
+  printf("\n");
   print_bmp_info_header(info_header);
+  printf("\n");
 
   write_bmp_header(out_fp, header);
   write_bmp_info_header(out_fp, info_header);
 
-  size_t rowSize = info_header->biWidth * 3;
+  size_t paddinglessRowSize = info_header->biWidth * 3;
   size_t fullRowSize =
       (info_header->biBitCount * info_header->biWidth + 31) / 32 * 4;
 
   uint8_t *rowBuffer = malloc(sizeof(uint8_t) * fullRowSize);
-  uint8_t *grayedBuffer = malloc(sizeof(uint8_t) * fullRowSize);
+  uint8_t *grayedBuffer = NULL;
+  /* Pixel *rowBuffer = malloc(sizeof(uint8_t) * fullRowSize); */
+  /* Pixel *grayedBuffer = malloc(sizeof(uint8_t) * fullRowSize); */
 
   while (fread(rowBuffer, fullRowSize, 1, in_fp) == 1) {
-    grayLine(rowBuffer, rowSize);
-    fwrite(rowBuffer, fullRowSize, 1, out_fp);
+    grayedBuffer = grayLine(rowBuffer, paddinglessRowSize, fullRowSize);
+    /* struct Pixel *pixelRow = (struct Pixel *)rowBuffer; */
+
+    fwrite(grayedBuffer, fullRowSize, 1, out_fp);
+
+    for (int i = 0; i < paddinglessRowSize; i++) {
+    }
+    free(grayedBuffer);
   }
 
   free(header);
   free(info_header);
   free(rowBuffer);
-  free(grayedBuffer);
 }
